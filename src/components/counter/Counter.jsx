034853a03faad76c7f2d83342c98b99f040ee81a
Alperen Can <alperencaan@gmail.com>
2023-09-27 01:05:43 +0300

    initial commit

---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import {PropTypes} from 'prop-types'
+import './Counter.css'
+
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    function incrementCounterParentFunction(by) {
+        setCount(count + by);
+    }
+    
+    function decrementCounterParentFunction(by) {
+        setCount(count - by);
+    }
+
+    return (
+        <>
+            <span className="totalCount">{count}</span>
+            <CounterButton by={1} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction} />
+            <CounterButton by={2} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction} />
+            <CounterButton by={5} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction} />
+        </>
+    )
+}
+
+function CounterButton({by, incrementMethod, decrementMethod}) {
+
+    const [count, setCount] = useState(0);
+
+    function incrementCounterFunction() {
+        setCount(count + by)
+        incrementMethod(by)
+        
+        // console.log(count)
+    }
+
+    function decrementCounterFunction() {
+        setCount(count - by)
+        decrementMethod(by)
+    }
+
+    return (
+        <div className="Counter">
+            <span className="count">{count}</span>
+            <div>
+                <button className="counterButton"
+                        onClick={incrementCounterFunction}
+                >+{by}</button>
+                
+                <button className="counterButton"
+                        onClick={decrementCounterFunction}
+                >-{by}</button>
+            </div>
+        </div>
+    )
+}
+
+CounterButton.propTypes = {
+    by: PropTypes.number
+}
+
+CounterButton.defaultProps = {
+    by: 1
+}
---
+++ src/components/learning-examples/SecondComponent.jsx
@@ -0,0 +1,7 @@
+
+export default function SecondComponent() {
+  return (
+    <div className='SecondComponent'>Bu da Second Component</div>
+  )
+}
+
